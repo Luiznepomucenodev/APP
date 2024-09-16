b80acf08ad5aea554e1f91c24715a040feb56e79
Luiznepomucenodev <nepomucenodev@gmail.com>
2024-09-16 20:42:40 -0300

    add inquirer

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let meta = [{
-    value: "Estudar",
-    checked: true
-},
-{
-    value: "Treinar",
-    checked: false
-},
-{
-    value: "Codar",
-    checked: true
-},
-]
+function start(){
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+            break
+            case "listar":
+                console.log("Vamos listar")
+            break
+            case "sair":
+                return
+        }
+    }
+}
 
+start()
